1b7b4773a475d8424ec1c8e1be42826204ff7f08
ChristoT98 <christolakshan@gmail.com>
2021-07-13 23:58:48 +0530

    Created searchable dropdown data add part

--- src/screens/UserProfileScreen.js
+++ src/screens/UserProfileScreen.js
@@ -56,21 +56,6 @@ import SearchableDropdown from 'react-native-searchable-dropdown';
 
 const DATA_ARRAY = [];
 
-// const items = [
-//     //name key is must.It is to show the text in front
-//     { id: 1, name: 'angellist' },
-//     { id: 2, name: 'codepen' },
-//     { id: 3, name: 'envelope' },
-//     { id: 4, name: 'etsy' },
-//     { id: 5, name: 'facebook' },
-//     { id: 6, name: 'foursquare' },
-//     { id: 7, name: 'github-alt' },
-//     { id: 8, name: 'github' },
-//     { id: 9, name: 'gitlab' },
-//     { id: 10, name: 'instagram' },
-// ];
-
-
 function gramaniladariArray() {
     for (let i = 0; i < villageData.length; i++) {
         let gramaniladari = {};
@@ -79,6 +64,8 @@ function gramaniladariArray() {
         DATA_ARRAY.push(gramaniladari);
     }
 }
+
+
 
 const UserProfileScreen = (props) => {
     const navigation = useNavigation();
@@ -89,6 +76,7 @@ const UserProfileScreen = (props) => {
         }).catch(error => console.log(error))
     }
 
+
     const { storedCredentials, setStoredCredentials } = useContext(CredentialsContext);
     const { name, email, photoUrl, mobile, gramaNiladhariDivision } = storedCredentials;
 
@@ -98,24 +86,29 @@ const UserProfileScreen = (props) => {
     const [selectedGgramaNiladhariDivision, setSelectedGgramaNiladhariDivision] = useState("");
 
 
+    function gramaniladari() {
+        let gramaobj = {};
+        for (let i = 0; i < DATA_ARRAY.length; i++) {
+            if (DATA_ARRAY[i].name === selectedGgramaNiladhariDivision) {
+                gramaobj.id = i;
+                gramaobj.name = DATA_ARRAY[i].name;
+                break;
+            }
+        }
+        return gramaobj;
+    }
+
     const DB_URL = 'https://dry-waters-33546.herokuapp.com/user/';
 
     // set village data to villages array in loading screen
     useEffect(() => {
         gramaniladariArray();
-        //console.log(DATA_ARRAY);
+        console.log(DATA_ARRAY.length);
     }, []);
-
-
-    // fill villages into dropdown
-    // const DropDownData = villages.map((c) => {
-    //     return <Picker.Item key={c.gn_name} value={c.gn_name + " " + c.gn_number} label={c.gn_name + " " + c.gn_number} />;
-    // });
-
 
     //form handling
     const handleAddData = (values, setSubmitting) => {
-        
+
         handleMessage(null);
         //if
         if (!(mobile && gramaNiladhariDivision)) {
@@ -196,15 +189,14 @@ const UserProfileScreen = (props) => {
         <KeyboardAvoidingWrapper>
             <StyledContainer>
                 <InnerContainer>
-                    {/* <PageTitle>Welcome</PageTitle> */}
                     <SubTitle>Personal Details</SubTitle>
                     <Formik
                         initialValues={{ name: name, email: email, mobile: mobile, gramaNiladhariDivision: gramaNiladhariDivision }}
                         onSubmit={(values, { setSubmitting }) => {
                             values = { ...values };
                             values.gramaNiladhariDivision = selectedGgramaNiladhariDivision;
-                            console.log(values.gramaNiladhariDivision);
-                            if (values.name == '' || values.email == '' || values.mobile == '' || values.gramaNiladhariDivision == '') {
+                            console.log(values);
+                            if (values.name === '' || values.email === '' ||  values.mobile === '' || values.gramaNiladhariDivision === '') {
                                 handleMessage('Please fill all the fields');
                                 setSubmitting(false);
                             }
@@ -220,20 +212,18 @@ const UserProfileScreen = (props) => {
                                     Grama Niladhari Division
                                 </Text>
                                 <SearchableDropdown
-                                    onTextChange={(text) => console.log(text)}
-                                    //On text change listner on the searchable input
-                                    onItemSelect={(item) => setSelectedGgramaNiladhariDivision(item.id)}
-                                    //onItemSelect={(item) => console.log(item.name)}
-                                    //onItemSelect called after the selection from the dropdown
-                                    containerStyle={{}}
-                                    //suggestion container style
+                                    onTextChange={(text) => { }}
+                                    onItemSelect={(item) => { setSelectedGgramaNiladhariDivision(item.name);}}
+                                    containerStyle={{ padding: 5 }}
+                                    selectedItems={gramaniladari()}
                                     textInputStyle={{
                                         //inserted text style
-                                        padding: 15,
-                                        borderRadius: 5,
-                                        marginBottom: 10,
-                                        marginTop: 10,
-                                        backgroundColor: "#E5E7EB",
+                                        padding: 12,
+                                        borderWidth: 1,
+                                        borderColor: '#ccc',
+                                        backgroundColor: '#FAF7F6',
+                                        color: '#774645',
+                                        fontSize: 24,
                                     }}
                                     itemStyle={{
                                         //single dropdown item style
@@ -242,10 +232,11 @@ const UserProfileScreen = (props) => {
                                         backgroundColor: '#FAF9F8',
                                         borderColor: '#bbb',
                                         borderWidth: 1,
+                                        color: 'red',
                                     }}
                                     itemTextStyle={{
                                         //text style of a single dropdown item
-                                        color: '#222',
+                                        color: 'green',
                                     }}
                                     itemsContainerStyle={{
                                         //items container style you can pass maxHeight
@@ -254,80 +245,12 @@ const UserProfileScreen = (props) => {
                                     }}
                                     items={DATA_ARRAY}
                                     //mapping of item array
-                                    //defaultIndex={2}
-                                    //default selected item index
-                                    placeholder="Search Grama Niladhari Division"
-                                    //place holder for the search input
-                                    resetValue={false}
-                                    //reset textInput Value with true and false state
-                                    //underlineColorAndroid="transparent"
-                                //To remove the underline from the android input
-                                />
-                                {/* <View style={styles.lblTextContainer}>
-                                    <Text style={styles.lblText}>Grama Niladhari Division</Text>
-                                </View>
-                                <View style={styles.pickersParent}>
-                                    <View style={styles.countryPickerParent}>
-                                        <Picker
-                                            selectedValue={gramaNiladhariDivision}
-                                            style={styles.countryPicker}
-                                            onValueChange={(itemValue, itemIndex) => {
-                                                setSelectedVillage(itemValue);
-
-                                            }}
-                                            value={values.gramaNiladhariDivision}
-                                            mode="dialog"
-                                        >
-                                            <Picker.Item label="Select Grama Niladhari Division" value="" />
-                                            {DropDownData}
-                                        </Picker>
-                                    </View>
-                                </View> */}
-                                {/*
-                                <SearchableDropdown
-                                    onTextChange={(text) => console.log(text)}
-                                    //On text change listner on the searchable input
-                                    onItemSelect={(item) => {console.log(JSON.stringify(item.name))}}
-                                    //selectedValue= {gramaNiladhariDivision}
-                                    //value={values.gramaNiladhariDivision}
-                                    //onItemSelect called after the selection from the dropdown
-                                    containerStyle={{ padding: 5 }}
-                                    //suggestion container style
-                                    textInputStyle={{
-                                        //inserted text style
-                                        padding: 12,
-                                        borderWidth: 1,
-                                        borderColor: '#ccc',
-                                        backgroundColor: '#FAF7F6',
-                                    }}
-                                    itemStyle={{
-                                        //single dropdown item style
-                                        padding: 10,
-                                        marginTop: 2,
-                                        backgroundColor: '#FAF9F8',
-                                        borderColor: '#bbb',
-                                        borderWidth: 1,
-                                    }}
-                                    itemTextStyle={{
-                                        //text style of a single dropdown item
-                                        color: '#222',
-                                    }}
-                                    itemsContainerStyle={{
-                                        //items container style you can pass maxHeight
-                                        //to restrict the items dropdown hieght
-                                        maxHeight: '60%',
-                                    }}
-                                    items={items}
-                                    //mapping of item array
-                                    //defaultIndex={1}
+                                    //defaultIndex={7}
                                     //default selected item index
                                     placeholder="placeholder"
                                     //place holder for the search input
                                     resetValue={false}
-                                    //reset textInput Value with true and false state
-                                    underlineColorAndroid="transparent"
-                                //To remove the underline from the android input
-                                /> */}
+                                />
                             </View>
 
                             <MyTextInput
