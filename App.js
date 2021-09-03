8b24dd09fa48bd7209d4544ab81216877cbfb7c2
LalithK90 <asakahatapitiya@gmail.com>
2021-09-03 12:19:32 +0530

    code transfer to app.js

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Animated, StyleSheet, View, Platform } from "react-native";
 import { StatusBar } from 'react-native';
 import * as SplashScreen from "expo-splash-screen";
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
 
 import RootStackNavigator from './src/navigators/RootStackNavigator';
 import AppLoading from 'expo-app-loading';
@@ -27,10 +29,42 @@ const store = createStore(themeReducer);
 
 SplashScreen.preventAutoHideAsync().catch(() => { });
 
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+let valueSample = 1;
+
 export default function App () {
   const [appReady, setAppReady] = useState(false);
   const [storedCredentials, setStoredCredentials] = useState("");
   //const [storedLanguage, setStoredLanguage] = useState(en);
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, [expoPushToken]);
+
 
 
   const checkLoginCredentials = () => {
@@ -173,3 +207,55 @@ function AnimatedSplashScreen ({ children, image }) {
     </View>
   );
 }
+
+async function registerForPushNotificationsAsync () {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+
+    alert(token + " in side");
+    saveToken(token).then(status => setSendStatus(status));
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+  return token;
+}
+
+async function saveToken (expoPushToken) {
+  let status;
+  if (expoPushToken) {
+    await fetch("https://suwen-sitimu-notfication-api.herokuapp.com/api/save_token", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: expoPushToken,
+        appName: "Suwen_Sitimu",
+      }),
+    }).then(x => {
+      status = x.status;
+    });
+  }
+  return status;
+};
--- src/screens/LocalDataScreen.js
+++ src/screens/LocalDataScreen.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, ScrollView, Platform } from "react-native";
 import { useTheme } from "@react-navigation/native";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { RFPercentage } from "react-native-responsive-fontsize";
-import Constants from 'expo-constants';
-import * as Notifications from 'expo-notifications';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp, } from "react-native-responsive-screen";
 import AppLoading from 'expo-app-loading';
 import { useFonts, ExpletusSans_500Medium, } from '@expo-google-fonts/expletus-sans';
@@ -42,24 +40,8 @@ import NoNetworkConnection from "../components/NoNetworkConnection";
 //i18n.locale = Localization.locale;
 //i18n.fallbacks = true;
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
-
-let valueSample = 1;
 
 export default function LocalDataScreen (props) {
-  const [expoPushToken, setExpoPushToken] = useState('');
-  const [notification, setNotification] = useState(false);
-  const [sendStatus, setSendStatus] = useState('');
-
-  const notificationListener = useRef();
-  const responseListener = useRef();
-
   const [loading, setLoading] = useState(false);
   const [connectStatus, setConnectStatus] = useState(false);
   const [covidData, setData] = useState('');
@@ -69,7 +51,6 @@ export default function LocalDataScreen (props) {
   useEffect(() => {
     setLoading(true);
     registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-    saveToken(expoPushToken).then(status => setSendStatus(status));
     notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
       setNotification(notification);
     });
@@ -77,21 +58,6 @@ export default function LocalDataScreen (props) {
       console.log(response);
     });
 
-    getStatistical().then(json => {
-      let covidData = { };
-      covidData.update_date_time = json.data.update_date_time;
-      covidData.local_total_cases = json.data.local_total_cases;
-      covidData.local_active_cases = json.data.local_active_cases;
-      covidData.local_new_cases = json.data.local_new_cases;
-      covidData.local_total_number_of_individuals_in_hospitals = json.data.local_total_number_of_individuals_in_hospitals;
-      covidData.local_recovered = json.data.local_recovered;
-      covidData.local_deaths = json.data.local_deaths;
-      covidData.local_new_deaths = json.data.local_new_deaths;
-      covidData.daily_antigen_testing_data_date = json.data.daily_antigen_testing_data[0].antigen_count;
-      covidData.daily_antigen_testing_data_count = json.data.daily_antigen_testing_data[0].date;
-      setData(covidData);
-      setLoading(false);
-    });
     checkConnected().then(res => {
       setConnectStatus(res);
     });
@@ -99,7 +65,7 @@ export default function LocalDataScreen (props) {
       Notifications.removeNotificationSubscription(notificationListener.current);
       Notifications.removeNotificationSubscription(responseListener.current);
     };
-  }, [expoPushToken, sendStatus]);
+  }, []);
 
   let [fontsLoaded] = useFonts({
     ExpletusSans_500Medium,
@@ -108,9 +74,6 @@ export default function LocalDataScreen (props) {
   if (!fontsLoaded) {
     return <AppLoading />;
   } else {
-    console.log("return token : " + expoPushToken + " number " + valueSample);
-    alert("return token : " + expoPushToken + " number " + valueSample);
-    valueSample++;
     return (
       connectStatus ? (
         <View style={styles.fullPage}>
@@ -125,8 +88,6 @@ export default function LocalDataScreen (props) {
                 showsVerticalScrollIndicator={false}
               >
                 <Text style={[styles.subTitle, { color: colors.subTitleColor }]}>Sri Lanka</Text>
-                <Text style={{ color: colors.subTitleColor }}>expo token value {expoPushToken}</Text>
-
                 <View style={styles.tileParent}>
                   <View style={{ flexDirection: "row" }}>
                     <Tile
@@ -221,52 +182,6 @@ async function getStatistical () {
   return data;
 }
 
-async function saveToken (expoPushToken) {
-  let status;
-  await fetch("https://suwen-sitimu-notfication-api.herokuapp.com/api/save_token", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      token: expoPushToken,
-      appName: "Suwen_Sitimu",
-    }),
-  }).then(x => {
-    status = x.status;
-  });
-  return status;
-};
-async function registerForPushNotificationsAsync () {
-  let token;
-  if (Constants.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    alert(token + " in side");
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-  return token;
-}
 const styles = StyleSheet.create({
   fullPage: {
     flex: 1,
